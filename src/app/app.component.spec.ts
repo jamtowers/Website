4010f73fd4a7794590a6ddcd630f8f938f54878c
James Towers <Meandmy10@outlook.com>
2020-02-26 15:16:10 +1100

    Remove Old Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'OnlineResume'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('OnlineResume');
-  });
+  // it(`should have as title 'OnlineResume'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('OnlineResume');
+  // });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
